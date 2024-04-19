1d1a76ffa18541743860919df7eb24f0e802c875
Philipp Engel <mail@philippengel.de>
2024-04-19 16:17:53 +0200

    adds a ratopticon  wsgi module

--- ratopticon/__init__.py
+++ ratopticon/__init__.py
@@ -2,10 +2,7 @@ from flask import Flask
 
 from ratopticon import camera_ctrl
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    app.register_blueprint(camera_ctrl.bp)
-
-    return app
+app.register_blueprint(camera_ctrl.bp)
 
