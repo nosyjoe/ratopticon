9aebb68b516858d5d4dac890ccdabf881e601deb
Philipp Engel <mail@philippengel.de>
2026-02-04 00:07:33 +0100

    updates dependencies, writes requirements file and fixes Bootstrap

--- ratopticon/__init__.py
+++ ratopticon/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap5
+from flask_bootstrap import Bootstrap
 
 from ratopticon import camera_ctrl
 
 app = Flask(__name__)
-bootstrap = Bootstrap5(app)
+bootstrap = Bootstrap(app)
 app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 
 app.register_blueprint(camera_ctrl.bp)
